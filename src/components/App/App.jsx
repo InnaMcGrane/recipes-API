05b9ff406e7314e1c0e2361383b97c8e1fd6da41
InnaMcGrane <innamcgrane@gmail.com>
2025-01-08 07:13:50 +0000

    init app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./app.module.scss";
+import cn from "classnames";
+
+const App = () => {
+  return <div>my super App!!!</div>;
+};
+
+export default App;
---
+++ src/components/Recipe/Recipe.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import cn from 'classnames';
+import styles from "./recipe.module.scss"
+
+const Recipe = () => {
+  return (
+    <div className={cn(styles["recipe"])}>
+      <div className={cn(styles["recipe__img-wrapper"])}>
+        <img className={cn(styles["recipe__img"])} src="" alt="" />
+      </div>
+      <h2 className={cn(styles["recipe__title"])}>Lorem, ipsum dolor.</h2>
+      <div className={cn(styles["recipe__rating"])}>rating: 4.6</div>
+      <div className={cn(styles["recipe__tags"])}></div>
+    </div>
+  );
+}
+
+export default Recipe
+
+/* {
+  "recipes": [
+    {
+      "id": 1,
+      "name": "Classic Margherita Pizza",
+      "ingredients": [
+        "Pizza dough",
+        "Tomato sauce",
+        "Fresh mozzarella cheese",
+        "Fresh basil leaves",
+        "Olive oil",
+        "Salt and pepper to taste"
+      ],
+      "instructions": [
+        "Preheat the oven to 475°F (245°C).",
+        "Roll out the pizza dough and spread tomato sauce evenly.",
+        "Top with slices of fresh mozzarella and fresh basil leaves.",
+        "Drizzle with olive oil and season with salt and pepper.",
+        "Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.",
+        "Slice and serve hot."
+      ],
+      "prepTimeMinutes": 20,
+      "cookTimeMinutes": 15,
+      "servings": 4,
+      "difficulty": "Easy",
+      "cuisine": "Italian",
+      "caloriesPerServing": 300,
+      "tags": [
+        "Pizza",
+        "Italian"
+      ],
+      "userId": 45,
+      "image": "https://cdn.dummyjson.com/recipe-images/1.webp",
+      "rating": 4.6,
+      "reviewCount": 3,
+      "mealType": [
+        "Dinner"
+      ]
+    } */
